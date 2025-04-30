7841818144afb9b00269717dba31395268577f21
SarahMBF <sara.maged555@gmail.com>
2025-04-30 16:17:37 +0200

    Update MySQL_Database_Creation.sql

--- MySQL_Database_Creation.sql
+++ MySQL_Database_Creation.sql
@@ -5,31 +5,31 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema lab MySQL
+-- Schema lab_mysql
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema lab MySQL
+-- Schema lab_mysql
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `lab MySQL` DEFAULT CHARACTER SET utf8 ;
-USE `lab MySQL` ;
+CREATE SCHEMA IF NOT EXISTS `lab_mysql` DEFAULT CHARACTER SET utf8 ;
+USE `lab_mysql` ;
 
 -- -----------------------------------------------------
--- Table `lab MySQL`.`Salespersons`
+-- Table `lab_mysql`.`Salespersons`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab MySQL`.`Salespersons` (
-  ` staff_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`Salespersons` (
+  `staff_id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `store` VARCHAR(45) NULL,
-  PRIMARY KEY (` staff_id`))
+  PRIMARY KEY (`staff_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `lab MySQL`.`Customers`
+-- Table `lab_mysql`.`Customers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab MySQL`.`Customers` (
-  ` customer_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`Customers` (
+  `customer_id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `phone_number` INT NULL,
   `email` VARCHAR(45) NULL,
@@ -38,14 +38,14 @@ CREATE TABLE IF NOT EXISTS `lab MySQL`.`Customers` (
   `province` VARCHAR(45) NULL,
   `country` VARCHAR(45) NULL,
   `zip` INT NULL,
-  PRIMARY KEY (` customer_id`))
+  PRIMARY KEY (`customer_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `lab MySQL`.`Invoices`
+-- Table `lab_mysql`.`Invoices`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab MySQL`.`Invoices` (
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`Invoices` (
   `invoice_number` INT NOT NULL,
   `date` DATE NULL,
   `Salespersons_ staff_id` INT NOT NULL,
@@ -55,32 +55,32 @@ CREATE TABLE IF NOT EXISTS `lab MySQL`.`Invoices` (
   INDEX `fk_Invoices_Customers1_idx` (`Customers_ customer_id` ASC) VISIBLE,
   CONSTRAINT `fk_Invoices_Salespersons1`
     FOREIGN KEY (`Salespersons_ staff_id`)
-    REFERENCES `lab MySQL`.`Salespersons` (` staff_id`)
+    REFERENCES `lab_mysql`.`Salespersons` (`staff_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Invoices_Customers1`
     FOREIGN KEY (`Customers_ customer_id`)
-    REFERENCES `lab MySQL`.`Customers` (` customer_id`)
+    REFERENCES `lab_mysql`.`Customers` (`customer_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `lab MySQL`.`Cars`
+-- Table `lab_mysql`.`Cars`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab MySQL`.`Cars` (
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`Cars` (
   `VIN` VARCHAR(45) NOT NULL,
-  `manufacturer` VARCHAR(50) NULL,
-  `model` VARCHAR(50) NULL,
-  `year` INT NULL,
-  `color` VARCHAR(50) NULL,
+  `manufacturer` VARCHAR(45) NOT NULL,
+  `model` VARCHAR(45) NOT NULL,
+  `year` INT NOT NULL,
+  `color` VARCHAR(45) NOT NULL,
   `Invoices_invoice_number` INT NOT NULL,
   PRIMARY KEY (`VIN`),
   INDEX `fk_Cars_Invoices1_idx` (`Invoices_invoice_number` ASC) VISIBLE,
   CONSTRAINT `fk_Cars_Invoices1`
     FOREIGN KEY (`Invoices_invoice_number`)
-    REFERENCES `lab MySQL`.`Invoices` (`invoice_number`)
+    REFERENCES `lab_mysql`.`Invoices` (`invoice_number`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
